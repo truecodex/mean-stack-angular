151a947b63c1c9b78dbe36d3a7ecd91d89ad88e2
truecodex <70598352+truecodex@users.noreply.github.com>
2020-09-01 23:50:12 +0530

    added dashboard module

---
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuard } from '../auth/auth.guard';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+    canActivate: [AuthGuard]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class DashboardRoutingModule { }
